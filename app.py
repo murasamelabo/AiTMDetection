04e3ac7c3f0e2b5eac8f06dfc910fadae717ef2f
murasamelabo <murasamelabo@murasamelabo.com>
2024-04-01 16:02:48 +0900

    OK

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for,send_file)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/image.jpg')
+def serve_image():
+    referer = request.headers.get('Referer')
+    print(f"Referer: {referer}")
+    if referer:
+        if 'https://login.microsoftonline.com/' in referer:
+            
+            image_path = 'img/OK.jpg'
+        else:
+            image_path = 'img/NG.jpg'
+    else:
+        image_path =  image_path = 'img/NG.jpg'
+
+    if os.path.exists(image_path):
+        return send_file(image_path, mimetype='image/jpeg')
+    else:
+        return "Image not found", 404
 
 
 if __name__ == '__main__':
