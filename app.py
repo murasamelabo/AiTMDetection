f51b994621abd4a3ba93024379a6451c39c05e28
murasamelabo <murasamelabo@murasamelabo.com>
2024-04-02 09:27:59 +0900

    comment out unneccessary code

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.after_request
 def add_header(response):
